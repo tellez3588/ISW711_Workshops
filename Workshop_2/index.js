bd81c4da1519952ac86b27109f3f5915aedd77b4
tellez3588 <106943072+tellez3588@users.noreply.github.com>
2023-02-15 18:31:54 -0600

    Workshop 2

---
+++ Workshop_2/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const bodyParse = require("body-parser");
+const userRoute = require('./Controllers/teams');
+const teamModel = require("./Models/teams");
+
+app.use(bodyParse.json());
+app.use('/api',userRoute);
+const db = mongoose.connect("mongodb://127.0.0.1:27017/fifaApi");
+
+
+
+app.listen(3000, () => console.log("Fifa app listening on port 3000!!"))
